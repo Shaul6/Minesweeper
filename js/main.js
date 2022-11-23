63664fc60c1ebb6ebcf6e263ecaf4fe01e293a8d
Shaul Gabrieli <95301129+ShaulGabrieli@users.noreply.github.com>
2022-11-23 20:57:09 +0200

    I added medium and hard difficulties, changes in css

--- js/main.js
+++ js/main.js
@@ -5,14 +5,27 @@ var gLevel = {
     SIZE: 4,
     MINES: 2,
 }
+const EASY = {
+    SIZE: 4,
+    MINES: 2,
+}
+const MEDIUM = {
+    SIZE: 8,
+    MINES: 14,
+}
+const HARD = {
+    SIZE: 12,
+    MINES: 32,
+}
 
+var gBombsTillLose = 1
+var gFlagsNeededCount = gLevel.MINES
+var gRegularCells = gLevel.SIZE ** 2 - gLevel.MINES
 const gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0 }
 
 function initGame() {
     gBoard = buildBoard()
-    createRandomBomb(gLevel.MINES)
     renderBoard(gBoard)
-    setMinesNegsCount()
     console.log('dddd', gBoard)
 }
 
@@ -34,4 +47,26 @@ function buildBoard() {
         }
     }
     return board
+}
+
+function gameOver() {
+    const elHeader = document.querySelector('h1')
+    console.log(elHeader)
+    elHeader.innerHTML = 'Game Over!'
+}
+
+function checkGameOver() {
+    console.log('regular cell', gRegularCells)
+    console.log('flagsNeededCount', gFlagsNeededCount)
+    if (gRegularCells === 0 && gFlagsNeededCount === 0) console.log('victory')
+}
+
+function gameLevel(level) {
+    gLevel.SIZE = level.SIZE
+    gLevel.MINES = level.MINES
+    gGame.isOn = false
+    gBombsTillLose = 1
+    gFlagsNeededCount = gLevel.MINES
+    gRegularCells = gLevel.SIZE ** 2 - gLevel.MINES
+    initGame()
 }
--- js/utils.js
+++ js/utils.js
@@ -15,7 +15,6 @@ function renderBoard(board) {
             }
 
             var cellClass = getClassName({ i: i, j: j })
-            // console.log('cellClass:', cellClass)
 
             strHTML += `\t<td data-i=${i} data-j=${j} class="cell ${cellClass}"  onclick="cellClicked(${i},${j})"  oncontextmenu="javascript:cellMarked(this);return false;">${currCell}</td>`
         }
@@ -57,19 +56,47 @@ function countNeighborsMines(cellI, cellJ, mat) {
     }
     return neighborsCount
 }
-
+function MinesOnFirstClick(){
+    if(!gGame.isOn) {
+        gGame.isOn = true
+        createRandomBomb(gLevel.MINES)
+        setMinesNegsCount()
+        
+    }
+}
 function cellClicked(i, j) {
+    if(gBombsTillLose === 0) return
+    MinesOnFirstClick()
+    const cellClassName = getClassName({ i, j })
+    const elCell = document.querySelector('.'+ cellClassName)
+    elCell.style.backgroundColor= 'rgb(174, 173, 173)'
+    expandShown(gBoard,elCell, i, j)
     var currCell = gBoard[i][j]
+    if(currCell.isMarked) return
     if (currCell.isShown) return
     else {
         currCell.isShown = true
         if (currCell.isMine) {
             currCell = MINE
+            gBombsTillLose--
+            if(gBombsTillLose===0){
+                elCell.style.backgroundColor= 'red'
+            }
+            renderCell({ i, j }, currCell)
+            gameOver()
+           
         } else {
+            
+            gRegularCells--
             currCell.minesAroundCount === 0 ? (currCell = ' ') : (currCell = currCell.minesAroundCount)
+            renderCell({ i, j }, currCell)
+            checkGameOver()
+            
+            return
         }
-        renderCell({ i, j }, currCell)
+        
     }
+    
 }
 
 function createRandomBomb(num) {
@@ -85,17 +112,52 @@ function createRandomBomb(num) {
 
 function cellMarked(elCell) {
     const currCell = gBoard[elCell.dataset.i][elCell.dataset.j]
+    if(currCell.isShown) return
     const location = {
         i: elCell.dataset.i,
         j: elCell.dataset.j,
     }
+    
     if (!currCell.isMarked) {
+        gFlagsNeededCount--
         currCell.isMarked = true
         renderCell(location, FLAG)
     } else {
+        gFlagsNeededCount++
         currCell.isMarked = false
         renderCell(location, ' ')
     }
+    checkGameOver() 
+}
+
+function expandShown(board,elCell, i, j){
+    const mineNeighbors = countNeighborsMines(i, j, board)
+    if(mineNeighbors === 0){
+        clickOnNeighbors(i, j, board, elCell)
+            }
+        }
+        
+    
+
+
+
+function clickOnNeighbors(cellI, cellJ, mat, elCell) {
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= mat.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= mat[i].length) continue
+            var currCell  = mat[i][j]
+            const cellClassName = getClassName({ i, j })
+            const elCell = document.querySelector('.'+ cellClassName)
+            currCell.isShown = true
+            elCell.style.backgroundColor= 'rgb(174, 173, 173)'
+            gRegularCells--
+            currCell.minesAroundCount === 0 ? (currCell = ' ') : (currCell = currCell.minesAroundCount)
+            renderCell({ i, j }, currCell)
+        }
+    }
+    
 }
 
 function getRandomIntInclusive(min, max) {
